81e22b8dcce9330fe8420ad8ca720352fc2c4e93
Anil Kumar <444.kumarshubham@gmail.com>
2024-09-01 12:44:39 +0530

    first commit

---
+++ frontend/src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
+import ChatProvider from "./Context/ChatProvider";
+import { BrowserRouter } from "react-router-dom";
+
+// Create the root element
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render the application
+root.render(
+  <ChakraProvider>
+    <BrowserRouter>
+      <ChatProvider>
+        <App />
+      </ChatProvider>
+    </BrowserRouter>
+  </ChakraProvider>
+);
+
+// Measure performance if needed
+reportWebVitals();
